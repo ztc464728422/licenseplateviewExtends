0d25194ee0cc25d2c925487c347ab5bd355f23d7
zhutiechao <464728422@qq.com>
2020-07-17 16:00:26 +0800

    sec

---
+++ app/src/main/java/com/winterpei/licenseplateutil/MainActivity.java
@@ -0,0 +1,44 @@
+package com.winterpei.licenseplateutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import com.winterpei.LicensePlateView;
+
+public class MainActivity extends AppCompatActivity implements LicensePlateView.InputListener {
+
+    private LicensePlateView mPlateView;
+    private RelativeLayout mContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlateView = findViewById(R.id.activity_lpv);
+        mContainer = findViewById(R.id.main_rl_container);
+        mPlateView.setInputListener(this);
+
+        mPlateView.setKeyboardContainerLayout(mContainer);
+        mPlateView.showLastView();
+        mPlateView.hideLastView();
+        mPlateView.onSetTextColor(R.color.colorAccent);
+
+        mPlateView.setDefaultStr("新A");
+    }
+
+    @Override
+    public void inputComplete(String content) {
+
+    }
+
+    @Override
+    public void deleteContent() {
+
+    }
+
+    @Override
+    public void onHideSoftInputMethod() {
+
+    }
+
+}
